eb2c6d89057645107f15d760ca69b32aee0f64e2
Mingwei Zhang <mingwei@mwzhang.com>
2017-02-14 10:20:50 -0800

    by default output CDF to the gnuplot file.

--- src/main/java/com/mwzhang/java/cdf/Cdf.java
+++ src/main/java/com/mwzhang/java/cdf/Cdf.java
@@ -18,13 +18,13 @@ import java.util.TreeSet;
  */
 public class Cdf {
 
-    Frequency freq;
-    DescriptiveStatistics stats;
+    private Frequency freq;
+    private DescriptiveStatistics stats;
 
-    TreeSet<Double> listDoubles;
-    TreeSet<Integer> listIntegers;
+    private TreeSet<Double> listDoubles;
+    private TreeSet<Integer> listIntegers;
 
-    public Cdf() {
+    private Cdf() {
         // do something
         this.freq = new Frequency();
         this.stats = new DescriptiveStatistics();
@@ -32,7 +32,31 @@ public class Cdf {
         this.listIntegers = new TreeSet<>();
     }
 
-    public void readCsv (String filename, int col){
+    public static void main(String[] args) {
+
+        // take CSV file and column id as input.
+        if (args.length != 2) {
+            System.err.println("USAGE: java -jar javacdf.jar FILENAME COLUMN");
+            return;
+        }
+        String filename = args[0];
+        int col = Integer.valueOf(args[1]);
+
+
+        Cdf cdf = new Cdf();
+        cdf.readCsv(filename, col);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output-cdf.gnuplot"));
+            cdf.writeGnuplotTemplate(writer);
+            cdf.writeCsvResults(writer);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done.");
+    }
+
+    private void readCsv(String filename, int col) {
 
         // READING
         System.out.println("Reading CSV file...");
@@ -41,9 +65,9 @@ public class Cdf {
             BufferedReader reader = new BufferedReader(fr);
 
             String line;
-            while((line=reader.readLine())!=null){
+            while ((line = reader.readLine()) != null) {
                 String[] columns = line.split(",");
-                if(columns.length<col){
+                if (columns.length < col) {
                     System.err.println("ERR: file does not have enough columns");
                     return;
                 }
@@ -57,87 +81,33 @@ public class Cdf {
         }
     }
 
-    public void writeCsvResults(){
+    private void writeCsvResults(BufferedWriter writer) throws IOException {
         System.out.println("Outputting CSV file...");
-        try {
-            FileWriter fw = new FileWriter("output-cdf.csv");
-            BufferedWriter writer = new BufferedWriter(fw);
-            for(Pair<Double,Double> pair : getCdfFloat()){
-                writer.write(String.format("%f,%f\n",pair.getValue0(),pair.getValue1()));
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (Pair<Double, Double> pair : getCdfFloat()) {
+            writer.write(String.format("%f,%f\n", pair.getValue0(), pair.getValue1()));
         }
-
     }
 
-    public void writeGnuplotTemplate(){
+    private void writeGnuplotTemplate(BufferedWriter writer) throws IOException {
         System.out.println("Outputting Gnuplot file...");
-        try {
-            FileWriter fw = new FileWriter("output-cdf.gnuplot");
-            BufferedWriter writer = new BufferedWriter(fw);
-            writer.write(GnuplotUtil.gnuplotTemplate);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writer.write(GnuplotUtil.gnuplotTemplate);
     }
 
-    public static void main(String[] args) {
-
-        // take CSV file and column id as input.
-        if(args.length!=2){
-            System.err.println("USAGE: java -jar javacdf.jar FILENAME COLUMN");
-            return;
-        }
-        String filename = args[0];
-        int col = Integer.valueOf(args[1]);
-
-        Cdf cdf = new Cdf();
-        cdf.readCsv(filename, col);
-        cdf.writeCsvResults();
-        cdf.writeGnuplotTemplate();
-        System.out.println("Done.");
-    }
-
-    public void addInt(Integer x) {
-        freq.addValue(x);
-        stats.addValue(x);
-        listIntegers.add(x);
-    }
-
-    public void addFloat(double x) {
+    private void addFloat(double x) {
         freq.addValue(x);
         stats.addValue(x);
         listDoubles.add(x);
     }
 
-    public Pair<List<Integer>, List<Double>> getCdfInt() {
-        List<Double> percentiles = new ArrayList<>();
-        List<Integer> values = new ArrayList<>();
+    private List<Pair<Double, Double>> getCdfFloat() {
 
-        for (Integer n : listIntegers) {
-            percentiles.add(freq.getCumPct(n));
-            values.add(n);
-        }
-
-        return new Pair<>(values, percentiles);
-    }
-
-    public List<Pair<Double,Double>> getCdfFloat() {
-
-        List<Pair<Double,Double>> results = new ArrayList<>();
+        List<Pair<Double, Double>> results = new ArrayList<>();
 
         for (Double n : listDoubles) {
             Double p = freq.getCumPct(n);
-            results.add(new Pair<> (n,p));
+            results.add(new Pair<>(n, p));
         }
         return results;
     }
 
-    public void reset() {
-        stats.clear();
-        freq.clear();
-    }
 }
--- src/main/java/com/mwzhang/java/cdf/GnuplotUtil.java
+++ src/main/java/com/mwzhang/java/cdf/GnuplotUtil.java
@@ -1,17 +1,13 @@
 package com.mwzhang.java.cdf;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 /**
  * GnuPlot utility functions
  *
  * Created by mingwei on 11/6/16.
  */
-public class GnuplotUtil {
+class GnuplotUtil {
 
-    static public String gnuplotTemplate =
+    static String gnuplotTemplate =
                     "reset\n" +
                     "set term postscript eps color enhanced \"Helvetica\" 25\n" +
                     "#set ylabel \"CDF\"\n" +
@@ -20,7 +16,7 @@ public class GnuplotUtil {
                     "set yrange [-0.05:1.05]\n" +
                     "set datafile separator ','\n" +
                     "set output \"output-cdf.eps\"\n" +
-                    "plot \"output-cdf.csv\" using 1:2 title \"CDF\" with lines lw 5 lt 1;\n"
+                            "plot \"-\" using 1:2 title \"CDF\" with lines lw 5 lt 1;\n"
             ;
 
 }
